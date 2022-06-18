b93000b2669a4a4d0c91366cd6091c2c9d2d6cf6
opensas <opensas@gmail.com>
2022-06-18 02:04:41 -0300

    clean up - remove curriculumInfo.ts

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
   import Curriculum from './components/cv/Curriculum.svelte'
   
-  // import { gistId } from './data/curriculumInfo'
-  // import { info } from './data/curriculumInfo'
   // import { info } from './data/cv_adn'
+  // import { gistId } from './data/cv_sas'
   import { info } from './data/cv_sas'
   
 </script>
